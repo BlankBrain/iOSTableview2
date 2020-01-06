b0108e7c5d14144f3f4489beb0e814dddf77e03a
Mehedi Hasan <m.mehedi_hasan@hotmail.com>
2020-01-06 15:45:50 +0600

    Initial Commit

---
+++ Tableview2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tableview2
+//
+//  Created by Md. Mehedi Hasan on 6/1/20.
+//  Copyright © 2020 Md. Mehedi Hasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
