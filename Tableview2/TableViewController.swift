31744c3cb948a6681c800d5d5024ea427dd1ae80
Mehedi Hasan <m.mehedi_hasan@hotmail.com>
2020-01-06 15:50:11 +0600

    #commit1

---
+++ Tableview2/TableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TableViewController.swift
+//  Tableview2
+//
+//  Created by Md. Mehedi Hasan on 6/1/20.
+//  Copyright © 2020 Md. Mehedi Hasan. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
