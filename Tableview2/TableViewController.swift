e4efd088ac5a5ae85421b837a9628399d0736b7d
Mehedi Hasan <m.mehedi_hasan@hotmail.com>
2020-01-07 16:32:50 +0600

    complete

--- Tableview2/TableViewController.swift
+++ Tableview2/TableViewController.swift
@@ -8,23 +8,76 @@
 
 import UIKit
 
-class TableViewController: UIViewController {
+class TableViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
+    var myDictionary: [String: [String]] = ["a": ["apple" , "audi"], "b": ["belkin" , "bose"] , "c" : ["compaq" , "comcast"] , "d" : ["dell" , "dos"] , "e" : ["exon mobil" , "ericson"]]
+    
+    @IBOutlet weak var table1: UITableView!
+    @IBOutlet weak var table2: UITableView!
+    let cellIdentifiar : String = "cell"
+    var numberOfCellsTable1:Int = 0
+    var numberOfCellsTable2:Int = 0
+    var keyValue:String = "a"
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView.tag == 1 {
+            numberOfCellsTable1 = myDictionary.keys.count
+            return numberOfCellsTable1
+        }
+        else if tableView.tag == 2 {
+            numberOfCellsTable2 = myDictionary[keyValue]!.count
+            return numberOfCellsTable2
+        }
+        else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifiar, for: indexPath) as UITableViewCell
+        if tableView.tag == 1 {
+            cell.textLabel?.text =  Array(myDictionary.keys)[indexPath.row]
+            
+        }
+        else if tableView.tag == 2 {
+            cell.textLabel?.text = myDictionary[keyValue]?[indexPath.row ]
+        }
 
-        // Do any additional setup after loading the view.
+        return cell
+        
     }
     
 
-    /*
-    // MARK: - Navigation
+     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+ 
+        print(indexPath.row)
+         if tableView.tag == 1 {
+        keyValue = Array(myDictionary.keys)[indexPath.row]
+            
+        }
+        
+        table2.reloadData()
+                
+       }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+ 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        table1.delegate = self
+        table2.delegate = self
+        table1.dataSource = self
+        table2.dataSource = self
+       
+
+        
     }
-    */
+    
+
+
 
 }
+
+
